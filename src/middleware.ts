8dc14a3f9e6dac23217fdc9aa268d5c3962cb586
Artem <artemidqwerty69@gmail.com>
2024-12-10 17:15:23 +0300

    реализована система регистрации. небольшие правки в middleware и форме регистрации

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,67 +1,70 @@
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
-import axios, { AxiosError } from 'axios';
+import axios, { AxiosError } from "axios";
 
-import routes from './types/routes';
+import routes from "./types/routes";
 
 export async function middleware(req: NextRequest) {
     const url = req.nextUrl.clone();
     const serverApi = process.env.NEXT_PUBLIC_SERVER_API;
 
-    const accessToken = req.cookies.get('accessToken')?.value || 'null';
-    const refreshToken = req.cookies.get('refreshToken')?.value;
-    const userId = req.cookies.get('userId')?.value;
+    const accessToken = req.cookies.get("accessToken")?.value;
+    const refreshToken = req.cookies.get("refreshToken")?.value;
+    const userId = req.cookies.get("userId")?.value;
 
-    if (!refreshToken) {
-        return NextResponse.redirect(new URL('/auth', url));
+    if (!accessToken || !refreshToken) {
+        return NextResponse.redirect(new URL("/auth", url));
     }
 
     try {
         await axios.get(serverApi + routes.VALIDATE_TOKEN, {
             headers: {
-                Authorization: 'bearer ' + accessToken
-            }
+                Authorization: "bearer " + accessToken,
+            },
         });
+        console.log("ok");
 
         return NextResponse.next();
-
     } catch (error: AxiosError | any) {
+        console.log(error);
 
-        console.log(error.status)
-
-        if (error.status === 401 && refreshToken) {
+        if (error.response?.status === 401 && refreshToken) {
             try {
-                const response = await axios.post(serverApi + routes.GET_NEW_TOKEN, { refreshToken, userId }, {
-                    headers: {
-                        Authorization: 'bearer ' + accessToken,
-                        refreshToken: refreshToken,
-                        userId: userId
+                const response = await axios.post(
+                    serverApi + routes.VALIDATE_REFRESH_TOKEN,
+                    { refreshToken },
+                    {
+                        headers: {
+                            Authorization: "bearer " + accessToken,
+                            refreshToken: refreshToken,
+                            userId: userId,
+                        },
                     }
-                });
+                );
 
-                const { accessToken: newAccessToken, refreshToken: newRefreshToken } = response.data;
+                const {
+                    accessToken: newAccessToken,
+                    refreshToken: newRefreshToken,
+                } = response.data;
 
                 const res = NextResponse.next();
 
-                res.cookies.set('accessToken', newAccessToken);
-                res.cookies.set('refreshToken', newRefreshToken);
+                res.cookies.set("accessToken", newAccessToken);
+                res.cookies.set("refreshToken", newRefreshToken);
 
                 return res;
-            } catch (refreshError: AxiosError | any) {
-                console.log(refreshError.response);
+            } catch (refreshError) {
                 return NextResponse.redirect(new URL(`/auth`, url));
             }
         }
 
-        console.error('Ошибка при проверке access токена:', error);
-        return NextResponse.redirect(new URL(`/auth`, url));
+        console.error("Ошибка при проверке access токена:", error);
+        return NextResponse.redirect(new URL(`/test?err=${error}`, url));
     }
 }
 
 export const config = {
     // Допишите свои роуты кому надо
-    matcher: [
-        '/'
-    ]
+    matcher: ["/"],
 };
